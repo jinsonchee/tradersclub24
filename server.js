1f3fe21a80d1456e753e154bdeafd9e89840c72f
jinsonchee <contact.jincofficial@gmail.com>
2026-04-10 15:14:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
-const PORT = process.env.PORT || 80;
-app.listen(PORT, () => {
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
